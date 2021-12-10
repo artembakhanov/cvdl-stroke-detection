1131b27544e9399be24365d8bf2b58679b6d167f
artembakhanov <artembakhanov@gmail.com>
2021-12-10 15:26:42 +0300

    Add atlas dataset and datamodule

---
+++ src/datamodules/datasets/atlas_dataset.py
@@ -0,0 +1,103 @@
+import glob
+import os
+import shutil
+from typing import Optional, Callable
+from urllib.error import URLError
+
+import cv2
+from torchvision.datasets.utils import download_file_from_google_drive, extract_archive
+
+from src.datamodules.datasets.BaseDataset import BaseDataset
+from src.utils import utils
+import pathlib
+
+log = utils.get_logger(__name__)
+
+
+class AtlasDataset(BaseDataset):
+    file_name = "Brain_Data_reordered2.zip"
+    file_id = "10lbK8GkXVXVhW-tUJ55IVLpbVs4fJlDY"
+    classes = {'no_stroke': 0, 'stroke': 1}
+
+    def __init__(self,
+                 root: str,
+                 transform: Optional[Callable] = None,
+                 target_transform: Optional[Callable] = None,
+                 download: bool = False,
+                 orphan: bool = False,
+                 image_size: int = 64,
+                 debug: bool = False,
+                 rotate: bool = False,
+                 rot_seq_prob: float = 0.4,
+                 test: bool = False,
+                 sequence_size: int = 15):
+        super(AtlasDataset, self).__init__(root, transform, target_transform, download, orphan, image_size,
+                                           debug)
+
+        if orphan:
+            return
+        self.rotate = rotate
+        self.rot_seq_prob = rot_seq_prob
+        self.test = test
+        self.sequence_size = sequence_size
+
+        self.images_sequences = {}
+        self.idx_to_seq = {}
+        self.rot_seq_prob = rot_seq_prob
+        self.distributions = []
+        self.rotate = rotate
+
+        self.samples = []
+        self.idx_to_cls = []
+
+        self._get_samples()
+
+    def __getitem__(self, idx):
+        seq = []
+        for i in range(1, self.sequence_size + 1):
+            seq.append(cv2.imread(self.samples[idx] / f"{i}.png"))
+
+        seq = list(map(
+            lambda img: self.transform(img)["image"] if self.transform else img,
+            seq
+        ))
+
+        return seq, self.idx_to_cls[idx]
+
+    def __len__(self):
+        return len(self.samples)
+
+    def _get_samples(self):
+        root = pathlib.Path(self.root) / 'test' if self.test else 'train'
+
+        for cls in self.classes:
+            seqs = [f for f in (root / cls).iterdir() if f.is_dir()]
+            self.samples.extend(seqs)
+            self.idx_to_cls.extend([self.classes[cls]] * len(seqs))
+
+    def _download(self) -> None:
+        """Download the brain strokes data if it doesn't exist already."""
+
+        if self._check_exists():
+            log.info(f"{self.__class__.__name__} is already downloaded. No need to download")
+            return
+
+        # download files
+        try:
+            log.info("Downloading {}...".format(self.file_name))
+            download_file_from_google_drive(self.file_id, self._raw_folder, filename=self.file_name)
+            log.info("Extracting {}...".format(self.file_name))
+            extract_archive(os.path.join(self._raw_folder, self.file_name), self.root)
+
+            files = glob.glob(f"{self.root}/s0me_name/*")
+            for f in files:
+                shutil.move(f, self.root)
+
+            shutil.rmtree(f"{self.root}/s0me_name/")
+            self._create_meta()
+        except URLError as error:
+            log.error(
+                "Failed to download (trying next):\n{}".format(error)
+            )
+        finally:
+            pass
